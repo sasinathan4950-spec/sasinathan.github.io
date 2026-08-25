c41539664e9b1c4f825e05199ac18581d906ea0b
sasinathan4950-spec <sasinathan4950@gmail.com>
2026-08-25 17:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    AOS.init({
-        duration: 1000,
-        once: true
-    });
-
     new Typed("#typing", {
         strings: [
-            "Embedded Software Engineer",
             "Firmware Developer",
-            "Embedded C Programmer",
+            "Embedded Software Engineer",
             "STM32 Developer",
-            "ARM Cortex-M Enthusiast"
+            "ARM Cortex-M Programmer",
+            "Device Driver Developer"
         ],
-        typeSpeed: 70,
+        typeSpeed: 60,
         backSpeed: 40,
+        backDelay: 1500,
         loop: true
-    });
-
-    const topBtn = document.getElementById("topBtn");
-
-    window.addEventListener("scroll", () => {
-
-        if (window.scrollY > 300) {
-            topBtn.style.display = "block";
-        } else {
-            topBtn.style.display = "none";
-        }
-
-    });
-
-    topBtn.addEventListener("click", () => {
-
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-
     });
 
 });
